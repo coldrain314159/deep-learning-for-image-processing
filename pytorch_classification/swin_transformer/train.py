defe2e4c82246fd797a59b8bfb98875fa0f586b6
coldrain314159 <103657844@qq.com>
2022-09-07 16:08:59 +0800

    修改数据路径

--- pytorch_classification/swin_transformer/train.py
+++ pytorch_classification/swin_transformer/train.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 
+import warnings      
 import torch
 import torch.optim as optim
 from torch.utils.tensorboard import SummaryWriter
@@ -10,6 +11,7 @@ from my_dataset import MyDataSet
 from model import swin_tiny_patch4_window7_224 as create_model
 from utils import read_split_data, train_one_epoch, evaluate
 
+warnings.filterwarnings(action='ignore')
 
 def main(args):
     device = torch.device(args.device if torch.cuda.is_available() else "cpu")
@@ -115,10 +117,10 @@ if __name__ == '__main__':
     # 数据集所在根目录
     # https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz
     parser.add_argument('--data-path', type=str,
-                        default="/data/flower_photos")
+                        default="./../../data/flower_photos")
 
     # 预训练权重路径，如果不想载入就设置为空字符
-    parser.add_argument('--weights', type=str, default='./swin_tiny_patch4_window7_224.pth',
+    parser.add_argument('--weights', type=str, default='./weights/swin_tiny_patch4_window7_224.pth',
                         help='initial weights path')
     # 是否冻结权重
     parser.add_argument('--freeze-layers', type=bool, default=False)
